ed58b3878d19bcc81c198a54aebb3547eabc22d3
MalgAmoe <malgamoe@gmail.com>
2018-10-23 00:03:50 +0200

    added some kind of picking logic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     this.state = {
       heroes: {},
       heroesArray: [],
-      heroesPicked: 0,
+      heroesPicked: { key: null, heroId: 0 },
       heroesDiscovered: new Set()
     }
   }
@@ -36,31 +36,34 @@ class App extends Component {
     this.setState({ heroesArray })
   }
 
-  pickCard(heroId) {
+  pickCard(heroId, key) {
     const { heroesPicked, heroesDiscovered } = this.state
 
-    if (heroesDiscovered.has(heroId)) return this.setState({ heroesPicked: 0 })
-    if (heroesPicked === 0) return this.setState({ heroesPicked: heroId })
-    else if (heroesPicked === heroId) {
+    if (heroesDiscovered.has(heroId)) return this.setState({ heroesPicked: { key: null, id: 0 } })
+    if (heroesPicked.key === null) return this.setState({ heroesPicked: { key, heroId } })
+    else if (heroesPicked.heroId === heroId && heroesPicked.key !== key) {
       heroesDiscovered.add(heroId)
-      console.log('discovered', heroesDiscovered)
-      return this.setState({ heroesPicked: 0, heroesDiscovered })
+      return this.setState({ heroesPicked: { key: null, id: 0 }, heroesDiscovered })
     }
-    this.setState({ heroesPicked: 0 })
+    this.setState({ heroesPicked: { key: null, id: 0 } })
   }
 
   render() {
-    const { heroesArray, heroes } = this.state
+    const { heroesArray, heroes, heroesPicked, heroesDiscovered } = this.state
 
     return (
       <div className="App">
         {heroesArray.map((heroId, key) => {
           const hero = heroes[heroId]
+          const picked = heroesPicked.key === key
+          const discovered = heroesDiscovered.has(heroId)
           return  <HeroCard
             size={200}
             key={key}
             hero={hero}
-            shuffle={() => this.pickCard(heroId)}
+            picked={picked}
+            discovered={discovered}
+            pickCard={() => this.pickCard(heroId, key)}
           />
         })}
       </div>
--- src/atoms/heroCard.js
+++ src/atoms/heroCard.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
-const HeroCard = ({ hero, size, shuffle }) => {
+const HeroCard = ({ hero, size, pickCard, picked, discovered }) => {
+  const style = {
+    height: size,
+    width: size,
+    borderRadius: Math.round(0.25 * size),
+    opacity: discovered ? 0 : 1
+  }
   return (
     <img
-      style={{height: size, width: size, borderRadius: Math.round(0.25 * size)}}
+      style={style}
       alt={hero.name}
-      src={hero.thumbnail}
-      onClick={shuffle}
+      src={picked ? hero.thumbnail : 'https://vignette.wikia.nocookie.net/callofduty/images/4/48/Evil_smiley_face.jpg/revision/latest?cb=20100921012126' }
+      onClick={pickCard}
     />
-  )
+)
 }
 
 export default HeroCard
