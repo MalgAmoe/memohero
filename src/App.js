9f9eea4895cb313800c572bdd8cfaf64acee6ae1
MalgAmoe <malgamoe@gmail.com>
2018-10-22 21:35:15 +0200

    use service and save heroes in state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
-import md5 from 'blueimp-md5';
 import './App.css';
-
-const baseURL = 'https://gateway.marvel.com:443/v1/public/';
-const privateKey = 'e8b65f84acf29444acbc923a006b9c4963bfb1f6';
-const publicKey = '59a89e925adfc0807da3394a4528b889';
+import getHeroes from './services/getHeroes'
 
 class App extends Component {
-  componentDidMount() {
-    this.getHeroes()
+  constructor(props) {
+    super(props)
+    this.state = {
+      heroes: []
+    }
   }
-  getHeroes() {
-    const ts = Date.now();
-    const stringToHash = ts + privateKey + publicKey;
-    const hash = md5(stringToHash);
 
-    return fetch(`${baseURL}characters?limit=50&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-      .then(data => {
-        console.log('data', data)
-        return data.json()
-      })
+  componentDidMount() {
+    getHeroes()
+    .then(heroes => this.setState({ heroes }))
   }
 
   render() {
     return (
       <div className="App">
-      Heroes!
+        {this.state.heroes.map(hero => {
+          return  <img style={{height: 200, width:200, borderRadius: 50}} key={hero.id} alt={hero.name} src={hero.thumbnail} />
+        })}
       </div>
     );
   }
