e780f5af3b7517d7c4b5b0ce70cbb2346a47c596
MalgAmoe <malgamoe@gmail.com>
2018-10-22 22:24:46 +0200

    add some shuffle

--- src/App.js
+++ src/App.js
@@ -6,22 +6,47 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      heroes: {}
+      heroes: {},
+      heroesArray: []
     }
   }
 
   componentDidMount() {
     getHeroes()
-    .then(heroes => this.setState({ heroes }))
+    .then(heroes => {
+      this.setState({ heroes })
+      this.shuffle()
+    })
+  }
+
+  shuffle() {
+    const { heroes } = this.state
+    const heroesIds = Object.keys(heroes)
+    let heroesArray = heroesIds.concat(heroesIds)
+
+    for (var i = heroesArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = heroesArray[i];
+      heroesArray[i] = heroesArray[j];
+      heroesArray[j] = temp;
+    }
+
+    this.setState({ heroesArray })
   }
 
   render() {
-    const { heroes } = this.state
-    const heroesArray = Object.values(heroes)
+    const { heroesArray, heroes } = this.state
     return (
       <div className="App">
-        {heroesArray.map(hero => {
-          return  <img style={{height: 200, width:200, borderRadius: 50}} key={hero.id} alt={hero.name} src={hero.thumbnail} />
+        {heroesArray.map((heroId, key) => {
+          const hero = heroes[heroId]
+          return  <img
+            style={{height: 200, width:200, borderRadius: 50}}
+            key={key}
+            alt={hero.name}
+            src={hero.thumbnail}
+            onClick={() => this.shuffle()}
+          />
         })}
       </div>
     );
