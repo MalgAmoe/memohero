1be3475debb4a5e54965c345e3aef0ec049345b1
MalgAmoe <malgamoe@gmail.com>
2018-10-28 18:45:16 +0100

    shuffle array in utils

---
+++ src/utils/shuffleArray.js
@@ -0,0 +1,10 @@
+export default (array) => {
+  const newArray = [].concat(array)
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = newArray[i]
+    newArray[i] = newArray[j]
+    newArray[j] = temp
+  }
+  return newArray
+}
